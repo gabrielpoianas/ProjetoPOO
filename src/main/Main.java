4c12e9ae9a982daad400bf40df21ca9b5a81d290
Gabriel Poianas <gpoianas@gmail.com>
2024-06-24 00:19:27 -0300

    Implantacao dos requisitos da semana 4

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,10 +2,33 @@ package main;
 
 import modelo.Financiamento;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Main{
     public static void main(String[] args) {
-        Financiamento financiamento = new Financiamento();
-        financiamento.executarFinanciamento();
+        Locale.setDefault(Locale.US);
+
+        List<Financiamento> listaFinanciamentos = new ArrayList<>();
+
+        listaFinanciamentos.add(new Financiamento(450000,4,7.45));
+        listaFinanciamentos.add(new Financiamento(158900,15,11.45));
+        listaFinanciamentos.add(new Financiamento(860000,2,7.96));
+        listaFinanciamentos.add(new Financiamento(50000,20,12.50));
+
+        double somaImoveis = 0, somaFinanciamentos = 0;
+        for(Financiamento financiamento : listaFinanciamentos){
+            System.out.printf("Financiamento - valor do imóvel: R$%.2f, valor do financiamento: R$%.2f\n", financiamento.getValorImovel(), financiamento.calcularTotalFinanciamento());
+            somaImoveis += financiamento.getValorImovel();
+            somaFinanciamentos += financiamento.calcularTotalFinanciamento();
+        }
+        System.out.println("-----------------------------------------------------------------------------------");
+        System.out.printf("Valor total dos imóveis: R$%.2f, Valor total dos financiamentos: R$%.2f\n", somaImoveis, somaFinanciamentos);
+        System.out.println("-----------------------------------------------------------------------------------");
+
+
+
 
     }
 }
--- src/modelo/Financiamento.java
+++ src/modelo/Financiamento.java
@@ -2,25 +2,36 @@ package modelo;
 
 import util.InterfaceUsuario;
 
+
 public class Financiamento extends InterfaceUsuario {
     private double valorImovel;
     private int prazoFinanciamento;
     private double taxaJurosAnual;
 
+    public Financiamento(double valorImovel, int prazoFinanciamento, double taxaJurosAnual) {
+        this.valorImovel = valorImovel;
+        this.prazoFinanciamento = prazoFinanciamento;
+        this.taxaJurosAnual = taxaJurosAnual;
+    }
+
     public double getValorImovel() {
-        this.valorImovel =  super.pedirValorImovel();
         return this.valorImovel;
     }
 
-    public int getPrazoFinanciamento(){
-        this.prazoFinanciamento = super.pedirPrazoFinanciamento();
-        return this.prazoFinanciamento;
-    }
-
-    public double getTaxaJurosAnual() {
-        this.taxaJurosAnual = super.pedirTaxaJurosAnual();
-        return this.taxaJurosAnual;
-    }
+//    public double getValorImovel() {
+//        this.valorImovel =  super.pedirValorImovel();
+//        return this.valorImovel;
+//    }
+//
+//    public int getPrazoFinanciamento(){
+//        this.prazoFinanciamento = super.pedirPrazoFinanciamento();
+//        return this.prazoFinanciamento;
+//    }
+//
+//    public double getTaxaJurosAnual() {
+//        this.taxaJurosAnual = super.pedirTaxaJurosAnual();
+//        return this.taxaJurosAnual;
+//    }
 
     //Método para calcular o valor do pagamento mensal do financiamento
     public double calcularPagamentoMensal(){
@@ -32,15 +43,10 @@ public class Financiamento extends InterfaceUsuario {
         return this.calcularPagamentoMensal() * this.prazoFinanciamento * 12;
     }
 
-    //Método para mostrar mensagem na tela com os dados do financiamento (valor total do financiamento e valor do imóvel)
-    public void mostrarDadosFinanciamento(){
-        System.out.print("Valor do imóvel: R$" + this.valorImovel + ", valor do financiamento: R$" + this.calcularTotalFinanciamento());
-    }
 
-    public void executarFinanciamento(){
-        this.getValorImovel();
-        this.getPrazoFinanciamento();
-        this.getTaxaJurosAnual();
-        this.mostrarDadosFinanciamento();
-    }
+//    public void executarFinanciamento(){
+//        this.getValorImovel();
+//        this.getPrazoFinanciamento();
+//        this.getTaxaJurosAnual();
+//    }
 }
